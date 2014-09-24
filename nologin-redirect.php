f2942c77bdd9725c7f239a8fe754bdb00c2f2c66
yat8823jp <yat@yat-net.com>
2014-09-24 11:11:17 +0900

    orgprefix-nlr

--- nologin-redirect.php
+++ nologin-redirect.php
@@ -10,17 +10,17 @@ Description:非ログイン時、ログイン画面にリダイレクトさせ�
 Author YAT,Chiaki
 Version:1.0
 */
-function theme_name_script(){
+function nlr_theme_name_script(){
 	wp_enqueue_style( 'nologinredirect' , plugins_url('nologin-redirect-style.css', __FILE__ ) );
 }
-add_action( 'wp_enqueue_scripts' , 'theme_name_script');
+add_action( 'wp_enqueue_scripts' , 'nlr_theme_name_script');
 
-function no_login_redirect($content){
+function nlr_no_login_redirect($content){
 	if( !is_user_logged_in() ){
 		$url = ( empty( $_SERVER["HTTPS"] ) ? 'http://' : 'https://' ) . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
 		wp_safe_redirect( wp_login_url( $url ) );
 	}
 }
 
-add_action('wp_head', 'no_login_redirect');
+add_action('wp_head', 'nlr_no_login_redirect');
 ?>
